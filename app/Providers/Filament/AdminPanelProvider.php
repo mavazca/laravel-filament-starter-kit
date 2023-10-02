0ad6808b5b5881917301ad3ca245d2834bace3dc
Marcelo Vaz Castilho <marcelo.vaz.castilho@gmail.com>
2023-10-02 17:21:08 -0300

    Customizando o tema do filament

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -34,8 +34,13 @@ class AdminPanelProvider extends PanelProvider
             ->emailVerification()
             ->profile()
             ->colors([
-                'primary' => Color::Amber,
+//                'primary' => Color::hex('#014bde'),
+                'primary' => Color::Emerald,
             ])
+            ->font('Nunito')
+            ->viteTheme('resources/css/filament/admin/theme.css')
+            ->favicon(asset('images/favicon.svg'))
+            ->sidebarCollapsibleOnDesktop()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
